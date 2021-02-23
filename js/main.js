fcac9fe75bdf4920c9cd39fa3a851992bf095535
jsirna <whitewolfhunter13@yahoo.com>
2021-02-23 12:03:59 -0500

    added different save method

--- js/main.js
+++ js/main.js
@@ -10,10 +10,25 @@ function saveTabs() {
   }
 
   // make a blob
-  window.URL = window.URL || window.webkitURL;
-  var blob = new Blob([tabArray], { type: "text/plain;charset="+document.characterSet });
-  var url = window.URL.createObjectURL(blob);
-  saveAs(url,"doc.txt");
+  // window.URL = window.URL || window.webkitURL;
+  // var blob = new Blob([tabArray], { type: "text/plain;charset="+document.characterSet });
+  // var url = window.URL.createObjectURL(blob);
+  // saveAs(url,"doc.txt");
+
+  //trying a different way
+  var blob = new Blob([tabArray], {type: 'text/csv'});
+  const url = URL.createObjectURL(blob, { oneTimeOnly: true })
+  if(window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, filename);
+  }
+  else{
+      var elem = window.document.createElement('a');
+      elem.href = window.URL.createObjectURL(blob);
+      elem.download = 'saved_tabs.txt';        
+      document.body.appendChild(elem);
+      elem.click();        
+      document.body.removeChild(elem);
+  }
 
   //tabArray returns like this:
   // -------------,-------------,-------------
