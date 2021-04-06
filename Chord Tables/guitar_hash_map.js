9d6e0d3cf3765e6da75a8a1310ddeab920519274
jsirna <whitewolfhunter13@yahoo.com>
2021-04-06 18:30:57 -0400

    added all fixes

---
+++ Chord Tables/guitar_hash_map.js
@@ -0,0 +1,167 @@
+let guit_chord = new Map();
+
+// NOTES:
+// NEED TO ADD DIMINISHED NOTES TO A LOT OF THESE CHORD FAMILIES
+
+        //------------------------------------------------------------------------------------------------------------------------------
+        //--------------------------------------------------FIRST OCTAVE----------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------
+
+
+// A CHORD FIRST OCTAVE FAMILY:
+
+
+guit_chord.set('02220-', 'a_maj');
+guit_chord.set('22220-', 'a_6');
+guit_chord.set('32220-', 'a_7');
+guit_chord.set('556775', 'a_bar'); // no uke equivalent atm -- 
+guit_chord.set('03220-', 'a_sus');
+guit_chord.set('-5454-', 'a_dom_7_9'); // no uke equivalent atm
+guit_chord.set('01220-', 'a_min');
+guit_chord.set('', 'a_min_6'); // no uke equivalent atm
+guit_chord.set('31220-', 'a_min_7'); 
+
+
+
+// B CHORD FIRST OCTAVE FAMILY:
+
+guit_chord.set('24442-', 'b_maj');
+guit_chord.set('-4442-', 'b_maj_var_2'); // no uke equivalent atm
+guit_chord.set('20212-', 'b_7');
+guit_chord.set('23442-', 'b_min');
+guit_chord.set('7777--', 'b_min_7');
+guit_chord.set('778998', 'b_bar');
+guit_chord.set('--442-', 'b_5');
+guit_chord.set('22212-', 'b_9');
+
+
+// C CHORD FIRST OCTAVE FAMILY:
+
+guit_chord.set('01023-', 'c_maj');
+guit_chord.set('-5553-', 'c_maj_var_2'); // no uke equivalent atm
+guit_chord.set('3101--', 'c_min');
+guit_chord.set('34553-', 'c_min_bar'); // no uke equivalent atm
+guit_chord.set('01323-', 'c_7');
+guit_chord.set('6455--', 'c_min_7');
+// guit_chord.set('889 10 10 8', 'c_bar_var_2');   frets above the 9th fret  // no uke equivalent atm
+guit_chord.set('33323-', 'c_9');
+guit_chord.set('-8787-', 'c_9_var_2');
+
+
+// D CHORD FIRST OCTAVE FAMILY:
+
+guit_chord.set('2320--', 'd_maj');
+guit_chord.set('1320--', 'd_min');
+guit_chord.set('2120--', 'd_7');
+guit_chord.set('1120--', 'd_min_7');
+guit_chord.set('-7775-', 'd_maj_var_2');  // no uke equivalent atm
+guit_chord.set('55545-', 'd_9');
+guit_chord.set('2020--', 'd_6');
+
+
+// E CHORD FIRST OCTAVE FAMILY:
+
+guit_chord.set('001220', 'e_maj');
+guit_chord.set('031220', 'e_7');
+guit_chord.set('001020', 'e_7_var_2');
+guit_chord.set('000220', 'e_min');
+guit_chord.set('030220', 'e_min_7');
+guit_chord.set('-99970', 'e_maj_var_2');
+guit_chord.set('021220', 'e_6');
+guit_chord.set('002220', 'e_sus');
+
+
+// F CHORD FIRST OCTAVE FAMILY:
+
+guit_chord.set('-1233-', 'f_maj');
+guit_chord.set('112331', 'f_bar');
+guit_chord.set('142131', 'f_7');
+guit_chord.set('111331', 'f_min');
+guit_chord.set('141131', 'f_min_7');
+guit_chord.set('', 'f_maj_var_2');
+// guit_chord.set('- 10 10 10 8-', 'f_5'); CONTAINS DOUBLE DIGIT FRETS -- SHOULD IT BE IN FIRST OCTAVE???
+guit_chord.set('---331', 'f_5');
+guit_chord.set('11200-', 'f_6');
+guit_chord.set('4343--', 'f_dim');
+
+
+// G CHORD FIRST OCTAVE FAMILY
+
+guit_chord.set('300023', 'g_maj');
+guit_chord.set('100023', 'g_7');
+guit_chord.set('333553', 'g_min');
+guit_chord.set('363353', 'g_min_7');
+guit_chord.set('334553', 'g_maj_var_2'); // no ukulele equivalent atm
+guit_chord.set('-3232-', 'g_dom_7_9'); // no ukulele equivalent atm
+guit_chord.set('310023', 'g_sus');
+guit_chord.set('3232--', 'g_dim');
+
+
+        // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- Sharps and Flats First Octave -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+// F SHARP (G FLAT) FIRST OCTAVE FAMILY
+
+guit_chord.set('2234--', 'f_sharp_g_flat_maj');
+guit_chord.set('223442', 'f_sharp_g_flat_bar');
+guit_chord.set('223242', 'f_sharp_g_flat_7');
+guit_chord.set('222442', 'f_sharp_g_flat_min');
+guit_chord.set('252242', 'f_sharp_g_flat_min_7'); 
+guit_chord.set('- 11 11 11 9 - ', 'f_sharp_g_flat_maj_var_2'); // no ukulele equivalent atm -- DOUBLE DIGIT FRETS -- SHOULD IT BE CONSIDERED FIRST OCTAVE?
+guit_chord.set('---442', 'f_sharp_g_flat_5');
+guit_chord.set('-2121-', 'f_sharp_g_flat_9');
+guit_chord.set('999999', 'f_sharp_g_flat_11');
+
+
+// C SHARP (D FLAT) FIRST OCTAVE FAMILY
+
+guitar_chord.set('1213--', '');
+guitar_chord.set('7666- ', '');
+guitar_chord.set('0212--', '');
+guitar_chord.set('9 9 10 11 11 9', ''); // DOUBLE DIGIT FRETS
+guitar_chord.set('02122-', '');
+guitar_chord.set('--664-', ''); 
+guitar_chord.set('-2121-', ''); 
+guitar_chord.set('44434-', '');
+guitar_chord.set('', '');
+
+1213--
+7666- 
+0212--
+02122-
+--664-
+-2121-
+44434
+
+
+/*
+guitar_chord.set('', '');
+guitar_chord.set('', '');
+guitar_chord.set('', '');
+guitar_chord.set('', '');
+guitar_chord.set('', ''); 
+guitar_chord.set('', ''); 
+guitar_chord.set('', '');
+guitar_chord.set('', '');
+*/
+
+
+        //------------------------------------------------------------------------------------------------------------------------------
+        //-------------------------------------------------SECOND OCTAVE----------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------
+ 
+
+
+// E CHORD SECOND OCTAVE FAMILY:
+
+//guitar_chord.set('12 999--', 'e_var_3');  //FRETS ABOVE THE 9TH FRET -- no uke equivalent atm
+
+
+
+
+
+
+
+//document.write(g_chord.get(userInput));
+var hello = "hello!";
+
--- js/audio.js
+++
@@ -1,9 +0,0 @@
-//const selectedFile = document.getElementById('input').file;
-
-function handleFiles(event) {
-	var files = event.target.files;
-	$("#src").attr("src", URL.createObjectURL(files[0]));
-	document.getElementById("audioSmack").load();
-}
-
-document.getElementById("upload").addEventListener("change", handleFiles, false);
